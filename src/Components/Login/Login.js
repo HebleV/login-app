c157ba08d7ed53cc1fc9222dda08517e6855988a
HebleV <visheble@yahoo.com>
2020-08-03 23:55:11 +0530

    Added onChange funcn for login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 import { Col, Row } from "reactstrap";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 const Login = () => {
+    const [userid, setUserId] = useState('');
+    const [password, setPassword] = useState('');
+    const handleUsername = (e) => {
+        let loginVal = e.target.value;
+        setUserId(loginVal);
+    }
+
+    const handlePassword = (e) => {
+        let passwordVal = e.target.value;
+        setPassword(passwordVal);
+    }
+
   return (
     <Form>
       <Form.Row>
@@ -11,7 +23,7 @@ const Login = () => {
           Username
         </Form.Label>
         <Col lg={5}>
-          <Form.Control type="text" placeholder="Username" />
+          <Form.Control type="text" placeholder="Username" onChange={handleUsername} />
         </Col>
       </Form.Row>{" "}
       <br />
@@ -20,7 +32,7 @@ const Login = () => {
           Password
         </Form.Label>
         <Col lg={5}>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={handlePassword} />
         </Col>
       </Form.Row>{" "}
       <br />
